851d8a6e13ab6f4aee945145bcd70adf3940ec02
Dustin McGilvray <dustinmcgilvray@gmail.com>
2018-11-18 21:14:38 -0600

    SideNav built-out more. Commiting for Cody to start connections.

--- client/src/app/views/components/MainNav/MainNav.js
+++ client/src/app/views/components/MainNav/MainNav.js
@@ -1,8 +1,12 @@
 // React Import
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 // Materialize Imports
-import { SideNav, SideNavItem } from 'react-materialize';
+import { SideNav, SideNavItem, Footer } from 'react-materialize';
+
+// React Icons Imports
+import { GoMarkGithub } from "react-icons/go";
 
 // CSS Imports
 import "./MainNav.css";
@@ -17,24 +21,39 @@ class MainNav extends React.Component {
                 <SideNav
                 trigger={<div data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></div>}
                 fixed={true}
+                className="mainNav-bg"
                 // options={{
 
                 // }}
                 >
-                    <SideNavItem userView id='userProfile'
+                    <SideNavItem 
+                        userView 
+                        id='userProfile'
+                        className="black-text"
                         user={{
-                        background: 'black',
+                        backgroundColor: '',
                         image: '',
-                        name: 'John Doe',
-                        email: 'jdandturk@gmail.com'
+                        name: <span className='blk'>John Doe</span>,
+                        email: <span className='blk'>jdandturk@gmail.com</span>
                         }}
                     />
-                    <SideNavItem href='#!icon' icon='cloud'>First Link With Icon</SideNavItem>
-                    <SideNavItem href='#!second'>Second Link</SideNavItem>
                     <SideNavItem divider />
-                    <SideNavItem subheader>Subheader</SideNavItem>
-                    <SideNavItem waves href='#!third'>Third Link With Waves</SideNavItem>
-                </SideNav>
+                    <SideNavItem subheader className="mainNav-header">Administration</SideNavItem>
+                    <SideNavItem href='#!icon' icon='developer_board'>Dashboard</SideNavItem>
+                    <SideNavItem href='#!second' icon='event'>Trainings</SideNavItem>
+                    <SideNavItem href='#!second' icon='face'>Employees</SideNavItem>
+                    <SideNavItem href='#!second' icon='network_cell'>Network</SideNavItem>
+                    <SideNavItem divider />
+                    <SideNavItem subheader className="mainNav-header">User</SideNavItem>
+                    <SideNavItem href='#!icon' icon='dashboard'>My Dashboard</SideNavItem>
+                    <SideNavItem href='#!second' icon='event_note'>My Trainings</SideNavItem>
+                    <SideNavItem href='#!second' icon='folder'>My Documents</SideNavItem>
+                    <SideNavItem href='#!second' icon='people'>Contacts</SideNavItem> 
+                    <SideNavItem divider />
+                    <Footer
+                    copyrights= "Athena"
+                    className="mainNav-footer">Athena <GoMarkGithub /> </Footer>
+               </SideNav>
             </div>
 		);
 	}
