85036d302b3d0d1ed9713ea97376595956aa9240
Dustin McGilvray <dustinmcgilvray@gmail.com>
2018-11-28 08:49:48 -0600

    Commiting to pull from group.

--- client/src/site/Site.js
+++ client/src/site/Site.js
@@ -1,37 +1,41 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Site.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Site.css";
 
 class Site extends Component {
-	
-	constructor(props) {
-		super(props);
-		this.state = {
-			
-		};
-	}
-	
-	componentDidMount() {
-		//	Uncomment this code to redirect to app if user is already logged in
-/*
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    //	Uncomment this code to redirect to app if user is already logged in
+    /*
 		const jwtToken = localStorage.getItem('jwtToken');
 		if(jwtToken) {
 // 			this.props.history.push('/app');
 			window.location.href='/app';
 		}
 */
-	}
-	
-	render() {
-		return (
-			<div className='site-bg'>
-				<h1><Link to='/'>Athena</Link></h1>
-				<h2>Welcome to Athena</h2>
-				<p>Training management made easy...</p>
-				<p><Link to='/register'>Try Athena out for free!</Link> or <Link to='/login'>Login to your account</Link></p>
-			</div>
-		);
-	}
+  }
+
+  render() {
+    return (
+      <div className="site-bg">
+        <div className="container center">
+          <h1 className="athena-title">
+            <Link to="/">Athena</Link>
+          </h1>
+          <h2>Welcome to Athena</h2>
+          <p>Training management made easy...</p>
+          <p>
+            <Link to="/register">Try Athena out for free!</Link> or{" "}
+            <Link to="/login">Login to your account</Link>
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Site;
